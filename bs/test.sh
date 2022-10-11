ca9ec9556fb45bf43298b9b526c365d168919496
Róisín Grannell <r.grannell2@gmail.com>
2022-10-11 22:25:35 +0100

    added coverage command

--- bs/test.sh
+++ bs/test.sh
@@ -2,5 +2,3 @@
 
 export $(cat .env | xargs)
 deno test --coverage=./coverage -A tests/*-test.ts || { exit 0 }
-deno coverage ./coverage --lcov > /tmp/cs-coverage
-genhtml -o ./coverage-html /tmp/cs-coverage
